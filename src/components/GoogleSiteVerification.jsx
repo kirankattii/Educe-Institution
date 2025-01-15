f3461cc96417de42a094e1439ca9790c5d8acad4
kiran katti <kirankatti550@gmail.com>
2025-01-15 18:40:26 +0530

    updated

--- src/components/GoogleSiteVerification.jsx
+++ src/components/GoogleSiteVerification.jsx
@@ -5,6 +5,8 @@ const GoogleSiteVerification = () => {
   return (
     <Helmet>
       <meta name="google-site-verification" content="5swPBRYLUYAQgKheNWvN6MLM0FF0SIkelUsaSmW0Yqc" />
+      <meta name="google-site-verification" content="TyRDl7OUllPsk6iXi-3536sUsALBBGYrpfcZe8ZJ6VI" />
+
     </Helmet>
   );
 };
